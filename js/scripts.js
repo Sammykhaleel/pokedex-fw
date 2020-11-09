4b18bfeef6cf4a9d305bfacc3127353bb5005629
Fawwaz <you@example.com>
2020-11-09 16:39:20 -0700

    Implementing final version with an added search bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonRepository = (function () {
-  // variable name
+  // letiable name
   let pokemonList = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
@@ -65,8 +65,16 @@ let pokemonRepository = (function () {
       .then(function (details) {
         // Now we add the details to the pokemon
         pokemon.imageUrl = details.sprites.other.dream_world.front_default;
+        pokemon.weight = details.weight;
         pokemon.height = details.height;
-        pokemon.types = Object.keys(details.types);
+        pokemon.types = [];
+        details.types.forEach(function(itemType){
+          pokemon.types.push(itemType.type.name)
+        })
+        pokemon.abilities = [];
+        details.abilities.forEach(function(itemAbility){
+          pokemon.abilities.push(itemAbility.ability.name)
+        })
       })
       .catch(function (e) {
         console.error(e);
@@ -80,30 +88,30 @@ let pokemonRepository = (function () {
   }
   // show the modal content
   function showModal(item) {
-    var modalBody = $(".modal-body");
-    var modalTitle = $(".modal-title");
-    var modalHeader = $(".modal-header");
-    // var $modalContainer = $("#modal-container");
+    let modalBody = $(".modal-body");
+    let modalTitle = $(".modal-title");
+    let modalHeader = $(".modal-header");
+    // let $modalContainer = $("#modal-container");
     //clear existing content of the model
     // modalHeader.empty();
     modalTitle.empty();
     modalBody.empty();
 
     //creating element for name in modal content
-    var nameElement = $("<h1>" + item.name + "</h1>");
+    let nameElement = $("<h1>" + item.name + "</h1>");
     // // creating img in modal content
-    var imageElementFront = $('<img class="modal-img" style="width:50%">');
+    let imageElementFront = $('<img class="modal-img mx-auto" style="width:50%">');
     imageElementFront.attr("src", item.imageUrl);
-    var imageElementBack = $('<img class="modal-img" style="width:50%">');
+    let imageElementBack = $('<img class="modal-img" style="width:50%">');
     imageElementBack.attr("src", item.imageUrlBack);
     // //creating element for height in modal content
-    var heightElement = $("<p>" + "height : " + item.height + "</p>");
+    let heightElement = $("<p>" + "height : " + item.height + "</p>");
     // //creating element for weight in modal content
-    var weightElement = $("<p>" + "weight : " + item.weight + "</p>");
+    let weightElement = $("<p>" + "weight : " + item.weight + "</p>");
     // //creating element for type in modal content
-    var typesElement = $("<p>" + "types : " + item.types + "</p>");
+    let typesElement = $("<p>" + "types : " + item.types + "</p>");
     // //creating element for abilities in modal content
-    var abilitiesElement = $("<p>" + "abilities : " + item.abilities + "</p>");
+    let abilitiesElement = $("<p>" + "abilities : " + item.abilities + "</p>");
 
     modalTitle.append(nameElement);
     modalBody.append(imageElementFront);
@@ -124,7 +132,7 @@ let pokemonRepository = (function () {
   //   }
   // });
   // jQuery(window).on("keydown", e => {
-  //   var $modalContainer = $("#modal-container");
+  //   let $modalContainer = $("#modal-container");
   //   if (e.key === "Escape" && $modalContainer.hasClass("is-visible")) {
   //     hideModal();
   //   }
@@ -153,3 +161,24 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+function search() {
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  // li = ul.getElementsByTagName("");
+  li = ul.querySelectorAll(".card");
+  // console.log(li[0].querySelector(".card-body").querySelector(".card-title"));
+  for (i = 0; i < li.length; i++) {
+  // a = li[i].getElementsByTagName("a")[0];
+  a = li[i].querySelector(".card-body").querySelector(".card-title");
+  console.log(a.innerText);
+  txtValue = a.textContent || a.innerText;
+  if (txtValue.toUpperCase().indexOf(filter) > -1) {
+  li[i].style.display = "";
+  } else {
+  li[i].style.display = "none";
+  }
+  }
+  }
